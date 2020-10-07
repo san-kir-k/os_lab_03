2edcbf420b4029b6d1cb0e06fc69d0f2a18ea649
san-kir-k <dfomka@list.ru>
2020-10-07 22:45:19 +0300

    Добавлена возможность выбирать число потоков с помощью ключа запуска

--- src/lab3.c
+++ src/lab3.c
@@ -10,11 +10,11 @@ typedef struct container container;
 struct container {
     int s_size;
     int c_size;
-    int str_c;
-    int col_c;
     float* m;
     float* w;
     float* res;
+    int thread_id;
+    int threads;
 };
 
 void logs(char* str, int fd) {
@@ -60,64 +60,79 @@ int get_matrix(int s_size, int c_size, float* m) {
 // считаем одну ячейку
 void* cell_algo(void* arg) {
     container* c = arg;
-    int str_c = c->str_c, col_c = c->col_c, s_size = c->s_size, c_size = c->c_size;
+    int thread_id = c->thread_id, threads = c->threads,
+    s_size = c->s_size, c_size = c->c_size;
     float* m = c->m;
     float* w = c->w;
     float* res = c->res;
-    float new_val = 0.0;
     int dir[3] = {-1, 0, 1};
-    int count = 0;
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            if (((str_c + dir[i]) * c_size + col_c + dir[j] >= 0) &&
-            ((str_c + dir[i]) * c_size + col_c + dir[j] < c_size * s_size)) {
-                new_val += w[i * 3 + j] * m[(str_c + dir[i]) * c_size + col_c + dir[j]];
-                count++;
+    for (int t = thread_id; t < s_size * c_size; t += threads) {
+        int count = 0;
+        float new_val = 0.0;
+        int str_c = t / s_size;
+        int col_c = t % c_size; 
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (((str_c + dir[i]) * c_size + col_c + dir[j] >= 0) &&
+                ((str_c + dir[i]) * c_size + col_c + dir[j] < c_size * s_size)) {
+                    new_val += w[i * 3 + j] * m[(str_c + dir[i]) * c_size + col_c + dir[j]];
+                    count++;
+                }
             }
         }
+        new_val /= count;
+        res[str_c * c_size + col_c] = new_val;
     }
-    new_val /= count;
-    res[str_c * c_size + col_c] = new_val;
     pthread_exit(NULL);
 }
 
 // алгоритм 
-void algo(int s_size, int c_size, float* m, float* w, float* res, int K) {
+void algo(int s_size, int c_size, float* m, float* w, float* res, int K, int threads) {
+    int sectors = s_size * c_size / threads;
+    sectors += (s_size * c_size % threads == 0) ? 0 : 1;
     float tmp_m[s_size * c_size];
     deep_copy(s_size, c_size, m, tmp_m);
-    pthread_t tids[s_size * c_size];
+    pthread_t tids[threads];
     // нужно хранить передаваемые аргументы, иначе цикл отработает и переменная уровня цикла удалится
-    container c[s_size * c_size];
+    container c[threads];
     for (int _k = 0; _k < K; ++_k) {
-        for (int i = 0; i < s_size; ++i) {
-            for (int j = 0; j < c_size; ++j) {
-                // иначе цикл кончится, все удалится и гг
-                c[i * c_size + j].s_size = s_size;
-                c[i * c_size + j].c_size = c_size;
-                c[i * c_size + j].str_c = i;
-                c[i * c_size + j].col_c = j;
-                c[i * c_size + j].m = tmp_m;
-                c[i * c_size + j].w = w;
-                c[i * c_size + j].res = res;
-                if (pthread_create(&tids[i * c_size + j], NULL, cell_algo, &c[i * c_size + j]) != 0) {
-                    char* err = "Unable to create a thread!\n";
-                    logs(err, STDERR);
-                }
+        for (int i = 0; i < threads; ++i) {
+            c[i].s_size = s_size;
+            c[i].c_size = c_size;
+            c[i].m = tmp_m;
+            c[i].w = w;
+            c[i].res = res;
+            c[i].thread_id = i;
+            c[i].threads = threads;
+            if (pthread_create(&tids[i], NULL, cell_algo, &c[i]) != 0) {
+                char* err = "Unable to create a thread!\n";
+                logs(err, STDERR);
             }
         }
-        for (int i = 0; i < s_size; ++i) {
-            for (int j = 0; j < c_size; ++j) {
-                if (pthread_join(tids[i * c_size + j], NULL) != 0) {
-                    char* err = "Unable to wait a thread!\n";
-                    logs(err, STDERR);
-                }
+        for (int i = 0; i < threads; ++i) {
+            if (pthread_join(tids[i], NULL) != 0) {
+                char* err = "Unable to wait a thread!\n";
+                logs(err, STDERR);
             }
         }
         deep_copy(s_size, c_size, res, tmp_m);
     }
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        char* err = "Usage: ./executable -count_of_threads.\n";
+        logs(err, STDERR);
+        return 1;
+    } 
+    // конвертация строки в инт
+    char* e;
+    int threads = strtol(argv[1] + 1, &e, 10);
+    if (threads <= 0) {
+        char* err = "Count of threads must be positive.\n";
+        logs(err, STDERR);
+        return 1;
+    }
     char* input_msg = "Enter matrix dimensions, matrix, enter window, enter K.\n";
     logs(input_msg, STDOUT);
     int m_s_size = 0, m_c_size = 0, w_s_size = 3, w_c_size = 3, K = 0;
@@ -146,7 +161,7 @@ int main() {
         logs(err, STDERR);
         return 1;
     }
-    algo(m_s_size, m_c_size, m, w, result, K);
+    algo(m_s_size, m_c_size, m, w, result, K, threads);
     if (get_return == RI_EOF) {
         char endl = '\n';
         write(STDIN, &endl, 1);
@@ -154,5 +169,5 @@ int main() {
     char* output_msg = "Result matrix is:\n";
     logs(output_msg, STDOUT);
     print_matrix(m_s_size, m_c_size, result);
-    pthread_exit(NULL);
+    return 0;
 }
