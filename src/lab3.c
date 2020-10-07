dfe11812ada652950e1bae494f140489c0308457
san-kir-k <dfomka@list.ru>
2020-10-07 15:22:44 +0300

    Однопоточный алгоритм

--- src/lab3.c
+++ src/lab3.c
@@ -21,8 +21,17 @@ int read_dimension(int* d) {
     return RI_VALID;
 }
 
-int get_matrix(int str_c, int col_c, float m[str_c][col_c]) {
-    int scan_return = scan_matrix(str_c, col_c, m);
+void deep_copy(int s_size, int c_size,
+float to_copy[s_size][c_size], float copy[s_size][c_size]) {
+    for (int i = 0; i < s_size; ++i) {
+        for (int j = 0; j < c_size; ++j) {
+            copy[i][j] = to_copy[i][j];
+        }
+    }
+}
+
+int get_matrix(int s_size, int c_size, float m[s_size][c_size]) {
+    int scan_return = scan_matrix(s_size, c_size, m);
     if (scan_return == RF_INVALID || scan_return == RF_INVALID_EOF) {
         char endl = '\n';
         char* err = "Invalid matrix, try to enter again!\n";
@@ -37,25 +46,58 @@ int get_matrix(int str_c, int col_c, float m[str_c][col_c]) {
     return scan_return;
 }
 
+// считаем одну ячейку
+void cell_algo(int str_c, int col_c, int s_size, int c_size,
+float m[s_size][c_size], float w[3][3], float res[s_size][c_size]) {
+    float new_val = 0.0;
+    int dir[3] = {-1, 0, 1};
+    int count = 0;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (str_c + dir[i] >= 0 && col_c + dir[j] >= 0 && str_c + dir[i] < s_size && col_c + dir[j] < c_size) {
+                new_val += w[i][j] * m[str_c + dir[i]][col_c + dir[j]];
+                count++;
+            }
+        }
+    }
+    new_val /= count;
+    res[str_c][col_c] = new_val;
+}
+
+// алгоритм (пока однопоточный)
+void algo(int s_size, int c_size, float m[s_size][c_size],
+float w[3][3], float res[s_size][c_size], int K) {
+    float tmp_m[s_size][c_size];
+    deep_copy(s_size, c_size, m, tmp_m);
+    for (int _k = 0; _k < K; ++_k) {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                cell_algo(i, j, 3, 3, tmp_m, w, res);
+            }
+        }
+        deep_copy(s_size, c_size, res, tmp_m);
+    }
+}
+
 int main() {
     char* input_msg = "Enter matrix dimensions, matrix, enter window, enter K.\n";
     logs(input_msg, STDOUT);
-    int m_str_c = 0, m_col_c = 0, w_str_c = 3, w_col_c = 3, K = 0;
+    int m_s_size = 0, m_c_size = 0, w_s_size = 3, w_c_size = 3, K = 0;
     // ввод матрицы исходной
-    if (read_dimension(&m_str_c) != RI_VALID) {
+    if (read_dimension(&m_s_size) != RI_VALID) {
         return 1;
     }
-    if (read_dimension(&m_col_c) != RI_VALID) {
+    if (read_dimension(&m_c_size) != RI_VALID) {
         return 1;
     }
-    float m[m_str_c][m_col_c], result[m_str_c][m_col_c];
-    int get_return = get_matrix(m_str_c, m_col_c, m);
+    float m[m_s_size][m_c_size], result[m_s_size][m_c_size];
+    int get_return = get_matrix(m_s_size, m_c_size, m);
     if (get_return != RF_VALID) {
         return 1;
     }
     // ввод окна
-    float w[w_str_c][w_col_c];
-    get_return = get_matrix(w_str_c, w_col_c, w);
+    float w[w_s_size][w_c_size];
+    get_return = get_matrix(w_s_size, w_c_size, w);
     if (get_return != RF_VALID) {
         return 1;
     }
@@ -66,14 +108,13 @@ int main() {
         logs(err, STDERR);
         return 1;
     }
+    algo(m_s_size, m_c_size, m, w, result, K);
     if (get_return == RI_EOF) {
         char endl = '\n';
         write(STDIN, &endl, 1);
     }
-    // algo
     char* output_msg = "Result matrix is:\n";
     logs(output_msg, STDOUT);
-    print_matrix(m_str_c, m_col_c, m);
-    print_matrix(w_str_c, w_col_c, w);
+    print_matrix(m_s_size, m_c_size, result);
     return 0;
 }
